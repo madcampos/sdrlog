ebcb8d18ffe248ca059a3c7c792d85e813819f3e
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2018-03-05 16:36:02 -0400

    feat: Add serviceworker stub

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-//TODO: add service worker
 import './chrome.js';
-//import './gamepad.js';
+import './gamepad.js';
 import './itens.js';
+
+/*if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then((reg) => {
+		console.log('Service worker registered');
+		console.log(reg);
+	}).catch((err) => {
+		console.error('Error registering service worker');
+		console.error(err);
+	});
+}*/
---
+++ sw.js
@@ -0,0 +1,9 @@
+/**
+ * Refs:
+ * https://www.sitepoint.com/getting-started-with-service-workers/
+ * https://developers.google.com/web/tools/workbox/
+ * https://developers.google.com/web/fundamentals/primers/service-workers/
+ * https://css-tricks.com/serviceworker-for-offline/
+ * https://serviceworke.rs/
+ * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+ */
