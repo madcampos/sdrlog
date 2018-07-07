c5481f4f45ba96eee658c8c77983ee15c26b1fb9
madcampos <madcampos@outlook.com>
2018-07-06 23:13:06 -0400

    chore: Remove critical and move contents to main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-import './chrome.js';
-import './gamepad.js';
-import './itens.js';
+/*eslint-disable no-console*/
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then(() => {
+		console.log('Service worker registered, loading scripts...');
+	}).catch((err) => {
+		console.error('Error registering service worker.');
+		console.error(err);
+
+		console.log('Falling back to default load...');
+	});
+} else {
+	console.log('No service worker, falling back to default load...');
+}
