7e5b5af0710342d28de17970977beac88a340611
madcampos <madcampos@outlook.com>
2021-02-05 19:35:18 -0400

    fix: Cleanup asset list

--- build/asset-list.mjs
+++ build/asset-list.mjs
@@ -1,4 +1,3 @@
-/* eslint-env node */
 /**
  * @file Assets list to generate.
  * @author madcampos <madcampos@outlook.com>
@@ -18,25 +17,7 @@ const BASE_PATH = 'dist/img/icons';
  * @typedef {object} Asset
  * @property {string} resource The path of the resource. It may be a svg or a html. In both cases it will be rendered and a screenshot will be taken.
  *
- * @property {('web'|'android'|'ios'|'windows'|'safari')} [platform='web'] The platform this resource will be used.
- *
- * Can be one of the following:
- * - **web**: PWA Resources, declared nad used by a instalable app with a `manifest`.
- * - **android**: Resources used by cordova to build for Android.
- * - **ios**: Resources used by cordova to build for iOS.
- * - **windows**: Resources used by cordova to build for Windows or by legacy Edge and IE.
- * - **safari**: Resources for Safari Web Clips (icons, pinned tab icon and splash screen).
- *
- * @property {('icon'|'maskable'|'splash'|'notification')} [type='icon'] The place the asset will be used.
- *
- * Can be one of the following:
- * - **icon**: The item will be used as an icon.
- * - **maskable**: The item will be used as a maskable icon, where the image have a solid background and the device can cut parts of it.
- * - **splash**: The item will be used as a splash screen.
- * - **notification**: The item will be used for notifications.
- * - **screenshot**: The item will be used as a screenshot.
- *
- * @property {('light'|'dark'|'both')} [mode='both'] The mode of the asset, `'light'` or `'dark'`.
+ * @property {'light'|'dark'} [mode='light'] The mode of the asset, `'light'` or `'dark'`.
  *
  * @property {string} destination The destination folder of the generated assets.
  *
@@ -47,25 +28,18 @@ export default [
 	{
 		assets: [
 			{
-				fileName: 'favicon-16x16.png',
-				height: 16,
-				width: 16
-			},
-			{
-				fileName: 'favicon-32x32.png',
-				height: 32,
-				width: 32
-			},
-			{
-				fileName: 'favicon.ico',
-				height: 32,
-				width: 32
-			},
-			{
-				fileName: '../../favicon.ico',
-				height: 32,
-				width: 32
-			},
+				fileName: 'favicon.svg',
+				height: 128,
+				width: 128
+			}
+		],
+		destination: BASE_PATH,
+		platform: 'web',
+		resource: 'assets/logo.svg',
+		type: 'icon'
+	},
+	{
+		assets: [
 			{
 				fileName: 'android-chrome-192x192.png',
 				height: 192,
@@ -80,7 +54,27 @@ export default [
 		destination: BASE_PATH,
 		platform: 'web',
 		resource: 'assets/icon.html',
-		type: 'icon'
+		type: 'icon',
+		mode: 'light'
+	},
+	{
+		assets: [
+			{
+				fileName: 'android-chrome-192x192-dark.png',
+				height: 192,
+				width: 192
+			},
+			{
+				fileName: 'android-chrome-512x512-dark.png',
+				height: 512,
+				width: 512
+			}
+		],
+		destination: BASE_PATH,
+		platform: 'web',
+		resource: 'assets/icon.html',
+		type: 'icon',
+		mode: 'dark'
 	},
 	{
 		assets: [
@@ -98,6 +92,26 @@ export default [
 		destination: BASE_PATH,
 		platform: 'web',
 		resource: 'assets/icon-bg.html',
-		type: 'icon'
+		type: 'icon',
+		mode: 'light'
+	},
+	{
+		assets: [
+			{
+				fileName: 'android-chrome-maskable-192x192-dark.png',
+				height: 192,
+				width: 192
+			},
+			{
+				fileName: 'android-chrome-maskable-512x512-dark.png',
+				height: 512,
+				width: 512
+			}
+		],
+		destination: BASE_PATH,
+		platform: 'web',
+		resource: 'assets/icon-bg.html',
+		type: 'icon',
+		mode: 'dark'
 	}
 ];
