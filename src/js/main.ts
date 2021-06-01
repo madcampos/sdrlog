a65fc71c9e537ab740fc6016ccb9a6d64ccbcf93
madcampos <madcampos@outlook.com>
2021-06-01 02:21:14 -0400

    feat: Move main file to typescript

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,6 @@
 /* eslint-disable no-console*/
-/**
- * @file Service worker registration.
- * @author madcampos <madcampos@outlook.com>
- * @version 1.0.0
- */
+import '../components/components';
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(() => {
 		console.log('Service worker registered, loading scripts...');
@@ -13,3 +10,5 @@ if ('serviceWorker' in navigator) {
 } else {
 	console.log('No service worker, falling back to default load...');
 }
+
+// TODO: boot other components
