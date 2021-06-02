c6be459fb6c41e34e8e9d331d63b2354a068523b
madcampos <madcampos@outlook.com>
2021-06-02 00:03:01 -0400

    feat: Implement progress component

--- src/components/components.ts
+++ src/components/components.ts
@@ -2,3 +2,4 @@ import './dialog/dialog';
 import './dropdown-menu/dropdown-menu';
 import './dropdown-menu/dropdown-menu-item';
 import './menu-bar/menu-bar';
+import './progress/progress';
---
+++ src/components/progress/progress.ts
@@ -0,0 +1,81 @@
+export class ProgressOverlay extends HTMLElement {
+	#root: ShadowRoot;
+
+	#progress: HTMLProgressElement;
+
+	#count: HTMLSpanElement;
+
+	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+	#total = 100;
+
+	#current = 0;
+
+	constructor() {
+		super();
+		this.#root = this.attachShadow({ mode: 'closed' });
+
+		this.#root.innerHTML = `
+			<dialog>
+				<h1><slot name="title"></slot></h1>
+				<progress></progress>
+				<p><span></span></p>
+				<p><slot name="info"></slot></p>
+			</dialog>
+		`;
+
+		this.#progress = this.#root.querySelector('progress') as HTMLProgressElement;
+		this.#count = this.#root.querySelector('span') as HTMLSpanElement;
+	}
+
+	static createOverlay({ total, title, info }: { total?: number, title?: string, info?: string }) {
+		const overlay = new ProgressOverlay();
+
+		overlay.innerHTML = `
+			<span slot="title">${title ?? ''}</span>
+			<span slot="info">${info ?? ''}</span>
+		`;
+
+		// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+		if (total) {
+			overlay.setTotal(total);
+		} else {
+			overlay.setIndefinite();
+		}
+
+		document.body.appendChild(overlay);
+		overlay.show();
+
+		return overlay;
+	}
+
+	setIndefinite() {
+		this.#progress.max = 0;
+		this.#count.hidden = true;
+	}
+
+	setTotal(total: number) {
+		this.#total = total;
+		this.#progress.max = total;
+		this.#count.innerText = `${this.#current} / ${this.#total}`;
+	}
+
+	setValue(value: number) {
+		this.#current = value;
+		this.#count.innerText = `${this.#current} / ${this.#total}`;
+	}
+
+	increment() {
+		this.#current += 1;
+		this.#count.innerText = `${this.#current} / ${this.#total}`;
+	}
+
+	show() {
+		this.#root.querySelector('dialog')?.showModal();
+	}
+
+	close() {
+		this.#root.querySelector('dialog')?.close();
+	}
+}
+
+customElements.define('progress-overlay', ProgressOverlay);
