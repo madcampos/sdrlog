e879b4bd37650d293dd416a63f418e2d9c010eb7
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-06-11 18:37:14 -0400

    fix: Improve data typing

--- data/data.d.ts
+++ data/data.d.ts
@@ -1,31 +1,18 @@
-declare enum IsoCode {
-	'de-DE' = 'de-DE',
-	'fr-FR' = 'fr-FR',
-	'jp-JP' = 'jp-JP',
-	'es-ES' = 'es-ES',
-	'hu-HU' = 'hu-HU',
-	'it-IT' = 'it-IT',
-	'pt-BR' = 'pt-BR',
-	'cs-CZ' = 'cs-CZ',
-	'he-IL' = 'he-IL',
-	'pl-PL' = 'pl-PL',
-	'fi-FI' = 'fi-FI',
-	'en-US' = 'en-US'
-}
+export type IsoCode = 'de-DE' | 'fr-FR' | 'jp-JP' | 'es-ES' | 'hu-HU' | 'it-IT' | 'pt-BR' | 'cs-CZ' | 'he-IL' | 'pl-PL' | 'fi-FI' | 'en-US';
 
 export interface Material {
 	category: 'rulebook'|'sourcebook'|'mission'|'magazine'|'novel'|'videogame'|'tcg'|'boardgame'|'misc',
 	type: 'digital'|'print'|'scan'|'ocr'|'physical',
 	sku: string[],
 	name: string,
-	names?: Record<IsoCode, string>,
+	names?: Partial<Record<IsoCode, string>>,
 	description: string,
 	edition: number,
 	publisher: string[],
 	gameDate?: string,
 	releaseDate?: string[],
 	status?: 'missing'|'outofscope'|'canceled',
-	originalLanguage: string,
+	originalLanguage: IsoCode,
 	notes?: string,
 	links?: {
 		title: string,
