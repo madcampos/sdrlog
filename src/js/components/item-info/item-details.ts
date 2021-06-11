43e1d8f1dbe84c20fd0ec023d8c8cdc801faf47a
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-06-11 18:40:01 -0400

    feat: Refactor item details and add handlers to lists

--- src/js/components/item-info/item-details.ts
+++ src/js/components/item-info/item-details.ts
@@ -1,11 +1,13 @@
-import { getMaterial } from '../data-operations/idb-persistence';
 import type { ModalDialog } from '../dialog/dialog';
 import type { EditBox } from '../edit-box/edit-box';
 import type { EditList } from '../edit-box/edit-list';
 import type { EditSelect } from '../edit-box/edit-select';
 import type { EditText } from '../edit-box/edit-text';
-import detailsTemplate from './details-template';
+import type { IsoCode, Material } from '../../../../data/data';
+
+import detailsTemplate, { formatPublisher, formatReleaseDate, formatSku, formatTranslatedName } from './details-template';
 import { setMaterialDetails } from './build-details';
+import { getMaterial, saveMaterial } from '../data-operations/idb-persistence';
 
 export class ItemDetails extends HTMLElement {
 	static get observedAttributes() { return ['id']; }
@@ -19,17 +21,23 @@ export class ItemDetails extends HTMLElement {
 
 	#name: EditBox;
 	#sku: EditList;
+	#skuInput: HTMLInputElement;
 	#edition: EditBox;
 	#gameDate: EditBox;
 	#category: EditSelect;
 	#type: EditSelect;
 	#language: EditSelect;
 	#releaseDate: EditList;
+	#releaseDateInput: HTMLInputElement;
 	#publisher: EditList;
+	#publisherInput: HTMLSelectElement;
 	#status: EditSelect;
 	#names: EditList;
+	#namesLangInput: HTMLSelectElement;
+	#namesValueInput: HTMLInputElement;
 	#files: EditList;
 	#links: EditList;
+	#linksInput: HTMLInputElement;
 	#cover: HTMLImageElement;
 	#notes: EditText;
 	#description: EditText;
@@ -48,30 +56,101 @@ export class ItemDetails extends HTMLElement {
 		this.#saveButton = this.#root.querySelector('#save') as HTMLButtonElement;
 
 		this.#name = this.#root.querySelector('#name') as EditBox;
+
 		this.#sku = this.#root.querySelector('#sku') as EditList;
+		this.#skuInput = this.#root.querySelector('#sku input') as HTMLInputElement;
+
 		this.#edition = this.#root.querySelector('#edition') as EditBox;
 		this.#gameDate = this.#root.querySelector('#gamedate') as EditBox;
 		this.#category = this.#root.querySelector('#category') as EditSelect;
 		this.#type = this.#root.querySelector('#type') as EditSelect;
 		this.#language = this.#root.querySelector('#language') as EditSelect;
+
 		this.#releaseDate = this.#root.querySelector('#releasedate') as EditList;
+		this.#releaseDateInput = this.#root.querySelector('#releasedate') as HTMLInputElement;
+
 		this.#publisher = this.#root.querySelector('#publisher') as EditList;
+		this.#publisherInput = this.#root.querySelector('#publisher select') as HTMLSelectElement;
+
 		this.#status = this.#root.querySelector('#status') as EditSelect;
+
 		this.#names = this.#root.querySelector('#names') as EditList;
+		this.#namesLangInput = this.#root.querySelector('#names select') as HTMLSelectElement;
+		this.#namesValueInput = this.#root.querySelector('#names input') as HTMLInputElement;
+
 		this.#files = this.#root.querySelector('#files') as EditList;
+
 		this.#links = this.#root.querySelector('#links') as EditList;
+		this.#linksInput = this.#root.querySelector('#links') as HTMLInputElement;
+
 		this.#cover = this.#root.querySelector('#cover') as HTMLImageElement;
 		this.#notes = this.#root.querySelector('#notes') as EditText;
 		this.#description = this.#root.querySelector('#description') as EditText;
 
-		// TODO: add event listener to add items when the event from lists is fired
+		this.#sku.addEventListener('additem', () => {
+			const { value } = this.#skuInput;
+
+			this.#sku.insertAdjacentHTML('beforeend', formatSku(value));
+		});
+
+		this.#releaseDate.addEventListener('additem', () => {
+			const { value } = this.#releaseDateInput;
+
+			this.#releaseDate.insertAdjacentHTML('beforeend', formatReleaseDate(value));
+		});
+
+		this.#publisher.addEventListener('additem', () => {
+			const { value } = this.#publisherInput;
+
+			this.#publisher.insertAdjacentHTML('beforeend', formatPublisher(value));
+		});
+
+		this.#names.addEventListener('additem', () => {
+			const { value: lang } = this.#namesLangInput;
+			const { value: name } = this.#namesValueInput;
+
+			this.#names.insertAdjacentHTML('beforeend', formatTranslatedName(lang, name));
+		});
+
+		this.#links.addEventListener('additem', () => {
+			const { value } = this.#linksInput;
+
+			this.#links.insertAdjacentHTML('beforeend', formatReleaseDate(value));
+		});
 
 		this.#editButton.addEventListener('click', () => {
 			this.toggleEdit();
 		});
 
-		this.#saveButton.addEventListener('click', () => {
-			// TODO: save material
+		this.#saveButton.addEventListener('click', async () => {
+			let status: Material['status'] | undefined;
+			const names = {};
+
+			if (this.#status.value !== 'ok') {
+				status = this.#status.value as Material['status'];
+			}
+
+			this.#names.values.forEach((item) => {
+				const [lang, name] = item.split(' → ');
+
+				names[lang as IsoCode] = name;
+			});
+
+			await saveMaterial(this.id, {
+				name: this.#name.value,
+				sku: this.#sku.values,
+				edition: Number.parseInt(this.#edition.value),
+				gameDate: this.#gameDate.value,
+				category: this.#category.value as Material['category'],
+				type: this.#type.value as Material['type'],
+				originalLanguage: this.#language.value as Material['originalLanguage'],
+				releaseDate: this.#releaseDate.values,
+				publisher: this.#publisher.values,
+				status,
+				names,
+				notes: this.#notes.value,
+				description: this.#description.value
+			});
 		});
 	}
 
@@ -118,7 +197,6 @@ export class ItemDetails extends HTMLElement {
 		this.#publisher.edit = editingStatus;
 		this.#status.edit = editingStatus;
 		this.#names.edit = editingStatus;
-		this.#files.edit = editingStatus;
 		this.#links.edit = editingStatus;
 		this.#notes.edit = editingStatus;
 		this.#description.edit = editingStatus;
