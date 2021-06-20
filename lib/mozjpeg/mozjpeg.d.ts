10600b1d30d5a4168ca3d256b656db66fd65db24
madcampos <madcampos@outlook.com>
2021-06-20 07:21:00 -0400

    fix: Resolve some type errors

--- lib/mozjpeg/mozjpeg.d.ts
+++ lib/mozjpeg/mozjpeg.d.ts
@@ -4,7 +4,7 @@ declare type ImagePointer = number;
 
 export interface MozJPEGModule extends EmscriptenModule {
 	encode(image: BufferSource, imageWidth: number, imageHeight: number, inputChannels: number, outputOptions: MozJPEGOptions): ImagePointer,
-	getImage(imagePointer: ImagePointer): Buffer,
+	getImage(imagePointer: ImagePointer): ArrayBuffer,
 	freeImage(imagePointer: ImagePointer): void
 }
 
