00e4db1e944644908a0dfd5c69fd2e6640fbd059
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-06-29 14:27:58 -0400

    feat: Remove never implemented gamepad

--- src/js/components/gamepad/gamepad.ts
+++
@@ -1,8 +0,0 @@
-// TODO: implement gamepad loop
-// Directional: move focus from one card to another based on document.elementFromPoint
-// X: open/close selected card
-// Start: info
-// Y: search
-// Ref: https://developer.mozilla.org/en-US/docs/Web/API/Document/elementFromPoint
-// Ref: https://github.com/neogeek/gamepad.js/
-// Ref: https://github.com/samiare/Controller.js
