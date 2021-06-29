b8e6a807932ec0ac0806d316c17a5fa1ab96e0ff
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-06-29 14:27:37 -0400

    feat: Add base rendering of epub

--- lib/epub/epub.d.ts
+++ lib/epub/epub.d.ts
@@ -1,6 +1,4 @@
 import Book, { BookOptions } from "./types/book";
 
-export default ePub;
-
 declare function ePub(urlOrData: string | ArrayBuffer, options?: BookOptions) : Book;
 declare function ePub(options?: BookOptions) : Book;
--- src/js/components/epub/reader.ts
+++ src/js/components/epub/reader.ts
@@ -1,13 +1,44 @@
 import '../../../../lib/zip/jszip';
-import ePub from '../../../../lib/epub/epub';
+import '../../../../lib/epub/epub';
 
-// TODO: implement reader ui
+import type { BookOptions } from '../../../../lib/epub/types/book';
+import type Book from '../../../../lib/epub/types/book';
+
+declare function ePub(urlOrData: string | ArrayBuffer, options?: BookOptions): Book;
+
+const renderArea = document.querySelector('#book') as HTMLElement;
+const nextButton = document.querySelector('#next') as HTMLButtonElement;
+const prevButton = document.querySelector('#prev') as HTMLButtonElement;
+const tocSelect = document.querySelector('#toc') as HTMLSelectElement;
+
 document.querySelector('input')?.addEventListener('change', async (evt) => {
 	const input = evt.target as HTMLInputElement;
 	const [file] = Array.from(input.files ?? []);
 
 	const book = ePub(await file.arrayBuffer());
-	const rendition = book.renderTo('area', { flow: 'paginated', width: 600, height: 400 });
+	const { toc } = await book.loaded.navigation;
+	const rendition = book.renderTo(renderArea);
+
+	rendition.themes.register('dark', './css/components/reader/theme.css');
+	rendition.themes.select('dark');
+
+	toc.forEach((chapter) => {
+		const option = document.createElement('option');
+
+		option.textContent = chapter.label;
+		option.value = chapter.href;
+
+		tocSelect.appendChild(option);
+	});
+
+	tocSelect.addEventListener('change', async () => {
+		const url = tocSelect.value;
+
+		await rendition.display(url);
+	});
+
+	prevButton.addEventListener('click', async () => rendition.prev());
+	nextButton.addEventListener('click', async () => rendition.next());
 
 	await rendition.display();
 });
