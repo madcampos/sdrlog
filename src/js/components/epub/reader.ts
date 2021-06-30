0a1300275233686be6a50522655e52caf1210b70
madcampos <madcampos@outlook.com>
2021-06-30 00:26:09 -0400

    feat: Implement reader

--- src/js/components/epub/reader.ts
+++ src/js/components/epub/reader.ts
@@ -3,6 +3,9 @@ import '../../../../lib/epub/epub';
 
 import type { BookOptions } from '../../../../lib/epub/types/book';
 import type Book from '../../../../lib/epub/types/book';
+import type { NavItem } from '../../../../lib/epub/types/navigation';
+import { getFile } from '../data-operations/idb-persistence';
+import { getFilePermission } from '../files-reader/files-reader';
 
 declare function ePub(urlOrData: string | ArrayBuffer, options?: BookOptions): Book;
 
@@ -11,34 +14,67 @@ const nextButton = document.querySelector('#next') as HTMLButtonElement;
 const prevButton = document.querySelector('#prev') as HTMLButtonElement;
 const tocSelect = document.querySelector('#toc') as HTMLSelectElement;
 
-document.querySelector('input')?.addEventListener('change', async (evt) => {
-	const input = evt.target as HTMLInputElement;
-	const [file] = Array.from(input.files ?? []);
+document.querySelector('#open-book')?.addEventListener('click', async (evt) => {
+	try {
+		(evt.target as HTMLButtonElement).disabled = true;
 
-	const book = ePub(await file.arrayBuffer());
-	const { toc } = await book.loaded.navigation;
-	const rendition = book.renderTo(renderArea);
+		const url = new URL(window.location.toString());
+		const params = new URLSearchParams(url.search);
 
-	rendition.themes.register('dark', './css/components/reader/theme.css');
-	rendition.themes.select('dark');
+		if (!params.has('file')) {
+			throw new Error('Missing book file.');
+		}
 
-	toc.forEach((chapter) => {
-		const option = document.createElement('option');
+		const filePath = params.get('file') as string;
+		const handler = await getFile(filePath) as FileSystemFileHandle | undefined;
 
-		option.textContent = chapter.label;
-		option.value = chapter.href;
+		if (!handler) {
+			throw new Error('Book does not exist.');
+		}
 
-		tocSelect.appendChild(option);
-	});
+		await getFilePermission(handler);
 
-	tocSelect.addEventListener('change', async () => {
-		const url = tocSelect.value;
+		const file = await handler.getFile();
 
-		await rendition.display(url);
-	});
+		const book = ePub(await file.arrayBuffer());
+		const { toc } = await book.loaded.navigation;
+		const rendition = book.renderTo(renderArea, { width: '100%', height: '100%', flow: 'scrolled-doc' });
 
-	prevButton.addEventListener('click', async () => rendition.prev());
-	nextButton.addEventListener('click', async () => rendition.next());
+		rendition.themes.register('dark', './css/components/reader/theme.css');
+		rendition.themes.select('dark');
 
-	await rendition.display();
+		const appendOptions = (chapter: NavItem) => {
+			const option = document.createElement('option');
+
+			option.textContent = chapter.label;
+			option.value = decodeURI(chapter.href);
+
+			tocSelect.appendChild(option);
+
+			chapter.subitems?.forEach(appendOptions);
+		};
+
+		toc.forEach(appendOptions);
+
+		tocSelect.addEventListener('change', async () => {
+			const chapterHref = tocSelect.value;
+
+			await rendition.display(chapterHref);
+		});
+
+		prevButton.addEventListener('click', async () => {
+			await rendition.prev();
+			tocSelect.value = rendition.location.start.href;
+		});
+		nextButton.addEventListener('click', async () => {
+			await rendition.next();
+			tocSelect.value = rendition.location.start.href;
+		});
+
+		await rendition.display();
+		document.querySelector('#load-overlay')?.remove();
+	} catch (err) {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+		(document.querySelector('main') as HTMLElement).innerText = err?.message ?? err ?? 'Error';
+	}
 });
