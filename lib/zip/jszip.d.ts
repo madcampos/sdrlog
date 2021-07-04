140f09563b0d58080e1dbf2a276bfdf7067efa6e
madcampos <madcampos@outlook.com>
2021-07-04 16:27:10 -0400

    fix: Resolve linting issues

--- lib/zip/jszip.d.ts
+++ lib/zip/jszip.d.ts
@@ -144,109 +144,6 @@ declare namespace JSZip {
     }
 }
 
-declare namespace JSZip {
-    const files: {[key: string]: JSZip.JSZipObject};
-
-    /**
-     * Get a file from the archive
-     *
-     * @param Path relative path to file
-     * @return File matching path, null if no file found
-     */
-    function file(path: string): JSZip.JSZipObject | null;
-
-    /**
-     * Get files matching a RegExp from archive
-     *
-     * @param path RegExp to match
-     * @return Return all matching files or an empty array
-     */
-    function file(path: RegExp): JSZip.JSZipObject[];
-
-    /**
-     * Add a file to the archive
-     *
-     * @param path Relative path to file
-     * @param data Content of the file
-     * @param options Optional information about the file
-     * @return JSZip object
-     */
-    function file<T extends JSZip.InputType>(path: string, data: InputByType[T] | Promise<InputByType[T]>, options?: JSZip.JSZipFileOptions): typeof JSZip;
-    function file<T extends JSZip.InputType>(path: string, data: null, options?: JSZip.JSZipFileOptions & { dir: true }): typeof JSZip;
-
-    /**
-     * Returns an new JSZip instance with the given folder as root
-     *
-     * @param name Name of the folder
-     * @return New JSZip object with the given folder as root or null
-     */
-    function folder(name: string): typeof JSZip | null;
-
-    /**
-     * Returns new JSZip instances with the matching folders as root
-     *
-     * @param name RegExp to match
-     * @return New array of JSZipFile objects which match the RegExp
-     */
-    function folder(name: RegExp): JSZip.JSZipObject[];
-
-    /**
-     * Call a callback function for each entry at this folder level.
-     *
-     * @param callback function
-     */
-    function forEach(callback: (relativePath: string, file: JSZip.JSZipObject) => void): void;
-
-    /**
-     * Get all files which match the given filter function
-     *
-     * @param predicate Filter function
-     * @return Array of matched elements
-     */
-    function filter(predicate: (relativePath: string, file: JSZip.JSZipObject) => boolean): JSZip.JSZipObject[];
-
-    /**
-     * Removes the file or folder from the archive
-     *
-     * @param path Relative path of file or folder
-     * @return Returns the JSZip instance
-     */
-    function remove(path: string): typeof JSZip;
-
-    /**
-     * Generates a new archive asynchronously
-     *
-     * @param options Optional options for the generator
-     * @param onUpdate The optional function called on each internal update with the metadata.
-     * @return The serialized archive
-     */
-    function generateAsync<T extends JSZip.OutputType>(options?: JSZip.JSZipGeneratorOptions<T>, onUpdate?: OnUpdateCallback): Promise<OutputByType[T]>;
-
-    /**
-     * Generates a new archive asynchronously
-     *
-     * @param options Optional options for the generator
-     * @param onUpdate The optional function called on each internal update with the metadata.
-     * @return A Node.js `ReadableStream`
-     */
-    function generateNodeStream(options?: JSZip.JSZipGeneratorOptions<'nodebuffer'>, onUpdate?: OnUpdateCallback): ReadableStream;
-
-    /**
-     * Deserialize zip file asynchronously
-     *
-     * @param data Serialized zip file
-     * @param options Options for deserializing
-     * @return Returns promise
-     */
-    function loadAsync(data: InputFileFormat, options?: JSZip.JSZipLoadOptions): Promise<typeof JSZip>;
-
-    const support: JSZipSupport;
-    const external: {
-        Promise: PromiseConstructorLike;
-    };
-    const version: string;
-}
-
 declare class JSZip {
     /**
      * Create JSZip instance
@@ -260,6 +157,107 @@ declare class JSZip {
      * @param options Description of the serialized zip archive
      */
     constructor(data?: InputFileFormat, options?: JSZip.JSZipLoadOptions);
+
+    files: {[key: string]: JSZip.JSZipObject};
+
+    /**
+     * Get a file from the archive
+     *
+     * @param Path relative path to file
+     * @return File matching path, null if no file found
+     */
+    file(path: string): JSZip.JSZipObject | null;
+
+    /**
+     * Get files matching a RegExp from archive
+     *
+     * @param path RegExp to match
+     * @return Return all matching files or an empty array
+     */
+    file(path: RegExp): JSZip.JSZipObject[];
+
+    /**
+     * Add a file to the archive
+     *
+     * @param path Relative path to file
+     * @param data Content of the file
+     * @param options Optional information about the file
+     * @return JSZip object
+     */
+    file<T extends JSZip.InputType>(path: string, data: InputByType[T] | Promise<InputByType[T]>, options?: JSZip.JSZipFileOptions): typeof JSZip;
+    file<T extends JSZip.InputType>(path: string, data: null, options?: JSZip.JSZipFileOptions & { dir: true }): typeof JSZip;
+
+    /**
+     * Returns an new JSZip instance with the given folder as root
+     *
+     * @param name Name of the folder
+     * @return New JSZip object with the given folder as root or null
+     */
+    folder(name: string): typeof JSZip | null;
+
+    /**
+     * Returns new JSZip instances with the matching folders as root
+     *
+     * @param name RegExp to match
+     * @return New array of JSZipFile objects which match the RegExp
+     */
+    folder(name: RegExp): JSZip.JSZipObject[];
+
+    /**
+     * Call a callback function for each entry at this folder level.
+     *
+     * @param callback function
+     */
+    forEach(callback: (relativePath: string, file: JSZip.JSZipObject) => void): void;
+
+    /**
+     * Get all files which match the given filter function
+     *
+     * @param predicate Filter function
+     * @return Array of matched elements
+     */
+    filter(predicate: (relativePath: string, file: JSZip.JSZipObject) => boolean): JSZip.JSZipObject[];
+
+    /**
+     * Removes the file or folder from the archive
+     *
+     * @param path Relative path of file or folder
+     * @return Returns the JSZip instance
+     */
+    remove(path: string): typeof JSZip;
+
+    /**
+     * Generates a new archive asynchronously
+     *
+     * @param options Optional options for the generator
+     * @param onUpdate The optional function called on each internal update with the metadata.
+     * @return The serialized archive
+     */
+    generateAsync<T extends JSZip.OutputType>(options?: JSZip.JSZipGeneratorOptions<T>, onUpdate?: OnUpdateCallback): Promise<OutputByType[T]>;
+
+    /**
+     * Generates a new archive asynchronously
+     *
+     * @param options Optional options for the generator
+     * @param onUpdate The optional function called on each internal update with the metadata.
+     * @return A Node.js `ReadableStream`
+     */
+    generateNodeStream(options?: JSZip.JSZipGeneratorOptions<'nodebuffer'>, onUpdate?: OnUpdateCallback): ReadableStream;
+
+    /**
+     * Deserialize zip file asynchronously
+     *
+     * @param data Serialized zip file
+     * @param options Options for deserializing
+     * @return Returns promise
+     */
+    loadAsync(data: InputFileFormat, options?: JSZip.JSZipLoadOptions): Promise<typeof JSZip>;
+
+    support: JSZipSupport;
+    external: {
+        Promise: PromiseConstructorLike;
+    };
+    version: string;
 }
 
 declare function JSZip(): typeof JSZip;
