177ef49d336b4b882963eb990e5563c7aad4cbb5
madcampos <madcampos@outlook.com>
2021-07-18 21:13:28 -0400

    feat: Re-add gamepad navigation TODO

---
+++ src/js/components/gamepad/gamepad.ts
@@ -0,0 +1,26 @@
+// TODO: add gamepad navigation
+// Directional: move focus from one card to another based on document.elementFromPoint
+// A: open card
+// B: close card
+// Start: info?
+// Y: search
+
+// Design docs:
+// Ref: https://docs.microsoft.com/en-us/windows/apps/design/input/gamepad-and-remote-interactions
+// Ref: https://www.voorhoede.nl/en/blog/navigating-the-web-with-a-gamepad/
+
+// Case studies:
+// Ref: https://github.com/microsoft/TVHelpers/tree/master/tvjs
+// Ref: https://github.com/dmahajan980/gamepad-prototype
+
+// Low level helper api:
+// Ref: https://developer.mozilla.org/en-US/docs/Web/API/Document/elementFromPoint
+
+// Gamepad libs:
+// Ref: https://github.com/FThompson/Gamepads.js
+// Ref: https://github.com/neogeek/gamepad.js/
+// Ref: https://github.com/samiare/Controller.js
+// Ref: https://github.com/MaulingMonkey/mmk.gamepad
+// Ref: https://github.com/ArunMichaelDsouza/joypad.js
+// Ref: https://github.com/alvaromontoro/gamecontroller.js
+// Ref: https://github.com/Tom32i/gamepad.js
