a56c60ca85df6c09b16165f8956d4ec5121b3fd9
madcampos <madcampos@outlook.com>
2021-07-30 01:58:25 -0400

    feat: Add loading messages

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,15 +1,32 @@
 /* eslint-disable no-console*/
+(document.querySelector('#load-overlay progress') as HTMLProgressElement).max = 11;
+
+function updateLoadStatus(status: string) {
+	const text = document.querySelector('#load-overlay h1') as HTMLHeadingElement;
+	const progress = document.querySelector('#load-overlay progress') as HTMLProgressElement;
+
+	text.innerText = status;
+	progress.value += 1;
+}
+
+updateLoadStatus('Loading components.');
 import './components/components';
+
+updateLoadStatus('Adding menu bar.');
 import './components/menu-bar/main-menu-items';
+
+updateLoadStatus('Importing helper functions.');
 import { fetchItems } from './components/data-operations/data-import';
 import { getMaterialsBasicInfo } from './components/data-operations/idb-persistence';
 import { updateFiltersFromURL } from './components/search-box/update-filter';
-import { updateInfoBoxFromURL } from './components/menu-bar/info-box-url';
+import { updateInfoBoxFromURL } from './components/info-box/info-box';
 import { checkForMatchingId, updateItemModalFromURL } from './components/item-info/item-details-url';
 import { createComparer } from './components/intl/formatting';
 
 document.addEventListener('DOMContentLoaded', async () => {
 	if ('serviceWorker' in navigator) {
+		updateLoadStatus('Registering Service Worker.');
+
 		try {
 			await navigator.serviceWorker.register(`${import.meta.env.PUBLIC_URL}sw.js`);
 		} catch (err) {
@@ -17,12 +34,18 @@ document.addEventListener('DOMContentLoaded', async () => {
 		}
 	}
 
+	updateLoadStatus('Fetching items database.');
+
 	await fetchItems();
+
+	updateLoadStatus('Sorting materials.');
 
 	const sorter = createComparer();
 	const materials = (await getMaterialsBasicInfo()).sort(({ name: nameA }, { name: nameB }) => sorter(nameA, nameB));
 	let matchedId: string | null = null;
 	let matchedTitle = '';
+
+	updateLoadStatus('Adding materials to the display.');
 
 	for (const material of materials) {
 		const itemCard = document.createElement('item-card');
@@ -43,11 +66,16 @@ document.addEventListener('DOMContentLoaded', async () => {
 	}
 
 	if (matchedId) {
+		updateLoadStatus('Setting modal from URL.');
+
 		updateItemModalFromURL(matchedId, matchedTitle);
 	}
+
+	updateLoadStatus('Setting information from URL.');
 
 	updateInfoBoxFromURL();
 	updateFiltersFromURL();
 
+	updateLoadStatus('Done!');
 	document.querySelector('#load-overlay')?.remove();
 });
