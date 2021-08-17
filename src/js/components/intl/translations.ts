7fcfb1ff822826ac4cede98207b93c624237ff51
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2021-08-17 15:18:08 -0400

    feat: Update translation system

--- src/js/components/intl/translations.ts
+++ src/js/components/intl/translations.ts
@@ -1,10 +1,23 @@
-const availableLanguages = ['en', 'fr', 'es', 'pt-BR'];
+/* eslint-disable max-classes-per-file */
+const availableLanguages = {
+	en: 'en-US',
+	fr: 'fr-FR',
+	es: 'es-ES',
+	pt: 'pt-BR'
+};
 
 let loadedTranslations: Map<string, string>;
 
 function translateHtmlText(text: string) {
 	// eslint-disable-next-line prefer-named-capture-group, @typescript-eslint/no-use-before-define
 	return text.replaceAll(/\bt\(['"`](.+?)['"`]\)/gu, (_, original: string) => I18n.t([original]));
+}
+
+
+// FIXME: remove comment after this is available/merged: https://github.com/microsoft/TypeScript/pull/39664
+declare class Locale {
+	constructor(tag: string);
+	language: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
@@ -14,7 +27,10 @@ export class I18n {
 	}
 
 	static async setLanguage(language: string) {
-		const translationLanguage = availableLanguages.find((lang) => language.startsWith(lang)) ?? 'en';
+		// @ts-expect-error
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		const locale = new Intl.Locale(language) as Locale;
+		const [, translationLanguage] = Object.entries(availableLanguages).find(([lang]) => locale.language.startsWith(lang)) ?? [null, 'en-US'];
 
 		(document.querySelector('html') as HTMLHtmlElement).lang = translationLanguage;
 		localStorage.setItem('appLanguage', translationLanguage);
