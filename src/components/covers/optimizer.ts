115be41469a6a46a173fbe5af2667e9b62d44357
madcampos <madcampos@outlook.com>
2022-12-29 19:55:03 -0500

    feat: move mozjpeg module to use npm lib

--- src/components/covers/optimizer.ts
+++ src/components/covers/optimizer.ts
@@ -1,16 +1,14 @@
-import mozjpegModule from '../../../lib/mozjpeg/mozjpeg.js';
-import defaultEncodeOptions from '../../../lib/mozjpeg/options.js';
+import mozjpegModule from '@saschazar/wasm-mozjpeg';
+import defaultEncodeOptions from '@saschazar/wasm-mozjpeg/options';
 
 const IMAGE_CHANNELS = 4;
 
 export async function optimize(imageArray: BufferSource, { width, height }: { width: number, height: number }) {
 	const mozjpeg = await mozjpegModule();
 
-	const imagePointer = mozjpeg.encode(imageArray, width, height, IMAGE_CHANNELS, defaultEncodeOptions);
+	const result = mozjpeg.encode(imageArray, width, height, IMAGE_CHANNELS, defaultEncodeOptions);
 
-	const result = mozjpeg.getImage(imagePointer);
-
-	mozjpeg.freeImage(imagePointer);
+	mozjpeg.free();
 
 	return result;
 }
