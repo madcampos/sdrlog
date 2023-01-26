97cdda27a967c02d2de77701fc19f8bd4cfd33e7
madcampos <madcampos@outlook.com>
2023-01-26 18:31:27 -0500

    feat: reorganize translation code

--- src/js/intl/translations.ts
+++ src/js/intl/translations.ts
@@ -1,33 +1,54 @@
-const availableLocales = [
-	new Intl.Locale('en-US'),
-	new Intl.Locale('pt-BR'),
-	new Intl.Locale('es-ES'),
-	new Intl.Locale('fr-FR')
-];
+import enUSTranslation from '../../locales/en-US.json?url';
+import ptBRTranslation from '../../locales/pt-BR.json?url';
+import frFRTranslation from '../../locales/fr-FR.json?url';
 
 let loadedTranslations = new Map<string, string>();
 
-function translateHtmlText(text: string) {
-	// eslint-disable-next-line @typescript-eslint/no-use-before-define
-	return text.replaceAll(/\bt\(['"`](.+?)['"`]\)/gu, (_, original: string) => I18n.t([original]));
-}
-
 export class I18n {
+	static get defaultLocale() {
+		return new Intl.Locale('en-US');
+	}
+
+	static get availableLocales() {
+		return [
+			new Intl.Locale('en-US'),
+			new Intl.Locale('pt-BR'),
+			new Intl.Locale('fr-FR')
+		];
+	}
+
+	static get availableTranslations() {
+		return {
+			'en-US': enUSTranslation,
+			'pt-BR': ptBRTranslation,
+			'fr-FR': frFRTranslation
+		};
+	}
+
+	static getMatchingLocale(locale: Intl.Locale) {
+		const matchingLocale = I18n.availableLocales.find((availableLocale) => availableLocale.language === locale.language);
+
+		return matchingLocale ?? I18n.defaultLocale;
+	}
+
+	static #translateHtmlText(text: string) {
+		return text.replaceAll(/\bt\(['"`](.+?)['"`]\)/gu, (_, original: string) => I18n.t([original]));
+	}
+
 	static getLanguage() {
-		return localStorage.getItem('appLanguage') ?? navigator.language;
+		return localStorage.getItem('app-language') ?? navigator.language;
 	}
 
 	static async setLanguage(language: string) {
 		const locale = new Intl.Locale(language);
-		const translationLocale = availableLocales.find((availableLocale) => availableLocale.language === locale.language) ?? availableLocales[0];
+		const translationLocale = I18n.getMatchingLocale(locale);
 		const translationLanguage = `${translationLocale.language}-${translationLocale.region}`;
 
 		(document.querySelector('html') as HTMLHtmlElement).lang = translationLanguage;
-		localStorage.setItem('appLanguage', translationLanguage);
-		localStorage.setItem('translationLanguage', translationLanguage);
+		localStorage.setItem('app-language', translationLanguage);
 
 		try {
-			const request = await fetch(`${import.meta.env.APP_PUBLIC_URL}locales/${translationLanguage}/messages.json`);
+			const request = await fetch(I18n.availableTranslations[translationLanguage]);
 			const translationJson = await request.json() as Record<string, string>;
 
 			loadedTranslations = new Map(Object.entries(translationJson));
@@ -36,7 +57,6 @@ export class I18n {
 		}
 	}
 
-	// eslint-disable-next-line id-length
 	static t(strings: TemplateStringsArray | string[]) {
 		const [messageKey] = strings;
 		// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
@@ -49,14 +69,14 @@ export class I18n {
 		const translateChildren = (baseNode: Node) => {
 			baseNode.childNodes.forEach((node) => {
 				if (node.nodeType === Node.TEXT_NODE) {
-					node.textContent = translateHtmlText(node.textContent ?? '');
+					node.textContent = I18n.#translateHtmlText(node.textContent ?? '');
 				}
 
 				if (node.nodeType === Node.ELEMENT_NODE) {
 					translateChildren(node);
 
 					[...(node as Element).attributes].forEach((attribute) => {
-						attribute.value = translateHtmlText(attribute.value);
+						attribute.value = I18n.#translateHtmlText(attribute.value);
 					});
 				}
 			});
