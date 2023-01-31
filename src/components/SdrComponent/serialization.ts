5ef13c196b02b075b2614671e300fbb28e857086
madcampos <madcampos@outlook.com>
2023-01-31 04:15:21 -0500

    chore: remove leftover code

--- src/components/SdrComponent/serialization.ts
+++ src/components/SdrComponent/serialization.ts
@@ -19,40 +19,36 @@ export type TemplateParser = (template: DocumentFragment) => { props: PropBindin
 
 export const templateParser: TemplateParser = (template) => {
 	const props: PropBinding[] = [];
-	const loopElments: Element[] = [];
 
 	const processNode = (node: Node) => {
 		if (node.nodeType === Node.ELEMENT_NODE) {
 			const { attributes } = node as Element;
-			const isInsideLoop = loopElments.includes(node.parentElement as Element);
 
-			if (!isInsideLoop) {
-				for (const attribute of [...attributes]) {
-					if (BIND_REGEX.test(attribute.name)) {
-						const attributeName = attribute.name.replace(BIND_REGEX, '');
-						const prop = attribute.value;
+			for (const attribute of [...attributes]) {
+				if (BIND_REGEX.test(attribute.name)) {
+					const attributeName = attribute.name.replace(BIND_REGEX, '');
+					const prop = attribute.value;
 
-						props.push({
-							prop,
-							element: node as HTMLElement,
-							attribute: attributeName,
-							type: 'attribute'
-						});
+					props.push({
+						prop,
+						element: node as HTMLElement,
+						attribute: attributeName,
+						type: 'attribute'
+					});
 
-						(node as Element).removeAttribute(attribute.name);
-					} else if (EVENT_REGEX.test(attribute.name)) {
-						const eventName = attribute.name.replace(EVENT_REGEX, '');
-						const handlerName = attribute.value;
+					(node as Element).removeAttribute(attribute.name);
+				} else if (EVENT_REGEX.test(attribute.name)) {
+					const eventName = attribute.name.replace(EVENT_REGEX, '');
+					const handlerName = attribute.value;
 
-						props.push({
-							prop: handlerName,
-							element: node as HTMLElement,
-							attribute: eventName,
-							type: 'event'
-						});
+					props.push({
+						prop: handlerName,
+						element: node as HTMLElement,
+						attribute: eventName,
+						type: 'event'
+					});
 
-						(node as Element).removeAttribute(attribute.name);
-					}
+					(node as Element).removeAttribute(attribute.name);
 				}
 			}
 		}
