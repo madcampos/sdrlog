80d985da8be59444fc7df82f7bbfd934074e9c56
madcampos <madcampos@outlook.com>
2023-02-08 04:06:20 -0500

    feat: migrat first components

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<div id="app">{{ foo }}</div>
+</template>
+
+<script setup lang="ts">
+	const foo = 'foo';
+
+	// TODO: load data from db and fetch
+	// TODO: iterate over data and create cards
+</script>
