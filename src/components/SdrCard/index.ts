907b5329b34c86fe6a54e3cf1375fc9604662a24
madcampos <madcampos@outlook.com>
2023-02-20 21:34:58 -0500

    fix: constructor missing default property

--- src/components/SdrCard/index.ts
+++ src/components/SdrCard/index.ts
@@ -34,7 +34,7 @@ export class SdrCard extends LitElement {
 
 	@property({ type: String }) declare thumbUrl: string;
 
-	constructor({ id, name, category, sku, type, edition, status }: Partial<CreateCardOptions>) {
+	constructor({ id, name, category, sku, type, edition, status }: Partial<CreateCardOptions> = {}) {
 		super();
 
 		this.id = id ?? '';
