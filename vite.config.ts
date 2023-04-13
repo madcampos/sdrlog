2d4793296eab3fededc34c01417fe748fe38c67e
madcampos <madcampos@outlook.com>
2023-04-13 03:35:59 -0400

    feat: update file icons

--- vite.config.ts
+++ vite.config.ts
@@ -181,7 +181,40 @@ export default defineConfig(({ mode }) => {
 						'.json',
 						'.sdr'
 					]
-				}
+				},
+				launch_type: 'single-client',
+				icons: [
+					{
+						src: 'icons/file/sdr-file-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/file/sdr-file-256.png',
+						sizes: '256x256',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/file/sdr-file-96.png',
+						sizes: '96x96',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/file/sdr-file-48.png',
+						sizes: '48x48',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/file/sdr-file-32.png',
+						sizes: '32x32',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/file/sdr-file-16.png',
+						sizes: '16x16',
+						type: 'image/png'
+					}
+				]
 			}
 		]
 	};
