99c93b29146ca1ab077666df989137b0e105df8d
madcampos <madcampos@outlook.com>
2023-06-19 20:09:28 -0400

    feat: add changelog stub

---
+++ src/components/SdrChangelog/index.ts
@@ -0,0 +1,3 @@
+// TODO: create a changelog component that will display the latest changes from the rss file
+// Ref: https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VersionHistory/explainer.md
+// Ref: https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VersionHistory/parsing-version-data.md
