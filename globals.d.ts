c1ab4147194a811a15a5d8aa503c65e35dd032c9
madcampos <madcampos@outlook.com>
2024-05-02 15:57:54 -0400

    chore: remove unused type

--- globals.d.ts
+++ globals.d.ts
@@ -2,8 +2,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 
-type Serializable = string | number | boolean | Serializable[] | { [key: string]: Serializable };
-
 interface ImportMetaEnv {
 	/** The app mode. Can be either `development` or `production`. */
 	readonly MODE: 'development' | 'production',
