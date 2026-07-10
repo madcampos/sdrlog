dd5237cdb168ed55e282a7e642b41900bc69373f
madcampos <609904+madcampos@users.noreply.github.com>
2026-07-10 19:12:10 -0400

    chore: remove leftover files

--- build/Missing-Images.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem '../public/data/' -Directory |
-Where-Object { $_.Name -ne '$schemas' } |
-ForEach-Object {
-	$IndexFile = Get-Content "$(Join-Path $_ -ChildPath 'index.json')" | ConvertFrom-Json
-
-	if (-not (Test-Path "$(Join-Path $_ -ChildPath $IndexFile.cover)" -PathType 'Leaf')) {
-		$_
-	}
-}
